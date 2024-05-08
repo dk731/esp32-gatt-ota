a85c7f5c0ed62d5bb0037c0d55fa1b68ca380d93
Demid Kaidalov <demid.kaidalov@gmail.com>
2024-05-08 22:40:04 +0300

    Finished working example with `esp-idf-svc` wrapper

--- dev/test.py
+++ dev/test.py
@@ -1,20 +1,78 @@
 import asyncio
-from bleak import BleakScanner
+from bleak import BleakClient, BleakScanner
 
 
 async def scan_ble_devices():
-    print("Scanning for BLE devices...")
-
     while True:
+        print("Scanning for BLE devices...")
         devices = await BleakScanner.discover()
         for device in devices:
-            if device.address == "7C:DF:A1:E8:7B:CE":
-                print(
-                    f"Device found: {device.name}, Address: {device.address}, Details: {device.details()}"
-                )
+            if device.name == "ESP32":
 
-        await asyncio.sleep(1)
+                print(f"Device found: {device.name}, Address: {device.address}")
+
+                async with BleakClient(device.address) as client:
+                    services = await client.get_services()
+                    print(
+                        f"Services and Characteristics for device at {device.address}:"
+                    )
+                    for service in services:
+                        print(f"\nService: {service}")
+                        characteristics = service.characteristics
+                        for characteristic in characteristics:
+                            print(f"  - Characteristic: {characteristic}")
+
+        print()
+        print()
+        # await asyncio.sleep(1)
 
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(scan_ble_devices())
+
+
+# import uuid
+
+
+# def uuid128_to_int128(uuid_str):
+#     # Parse the UUID string
+#     uuid_obj = uuid.UUID(uuid_str)
+
+#     # Get the components of the UUID
+#     time_low = uuid_obj.time_low
+#     time_mid = uuid_obj.time_mid
+#     time_hi_and_version = uuid_obj.time_hi_version
+#     clock_seq_hi_and_reserved = uuid_obj.clock_seq_hi_variant
+#     clock_seq_low = uuid_obj.clock_seq_low
+#     node = uuid_obj.node
+
+#     # Concatenate the components into a single int128
+#     int128_value = (
+#         (time_low << 96)
+#         | (time_mid << 80)
+#         | (time_hi_and_version << 64)
+#         | (clock_seq_hi_and_reserved << 56)
+#         | (clock_seq_low << 48)
+#         | node
+#     )
+
+#     return int128_value
+
+
+# def generate_new_uuid_and_int128():
+#     # Generate a new UUID
+#     new_uuid = uuid.uuid4()
+
+#     # Convert the UUID to a string
+#     uuid_str = str(new_uuid)
+
+#     # Convert the UUID string to int128
+#     int128_value = uuid128_to_int128(uuid_str)
+
+#     return new_uuid, int128_value
+
+
+# # Generate a new UUID and its corresponding int128 value
+# new_uuid, int128_value = generate_new_uuid_and_int128()
+# print("New UUID:", new_uuid)
+# print("Corresponding int128 value:", int128_value)
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,35 @@
+use std::{
+    borrow::Borrow,
+    sync::{Arc, Mutex},
+};
+
 use anyhow::{Ok, Result};
 // use esp32_nimble::{uuid128, BLEAdvertisementData, BLEDevice};
 use esp_idf_svc::{
     self,
+    bt::{
+        ble::{
+            gap::{AdvConfiguration, AppearanceCategory, EspBleGap},
+            gatt::{
+                server::{EspGatts, GattsEvent},
+                AutoResponse, GattCharacteristic, GattId, GattServiceId, GattStatus, Permission,
+                Property,
+            },
+        },
+        Ble, BtMode, BtUuid,
+    },
+    hal::{
+        modem::{self, Modem},
+        peripheral::Peripheral,
+    },
+    nvs::{EspDefaultNvsPartition, EspNvsPartition, NvsDefault},
     sys::{esp_log_level_set, esp_log_level_t_ESP_LOG_NONE},
 };
 
-// use esp_idf_svc::bt::ble::gatt
+use esp_idf_svc::bt::BtDriver;
+
+const PROFILE_A_APP_ID: u16 = 0;
+const PROFILE_B_APP_ID: u16 = 1;
 
 fn main() -> Result<()> {
     // It is necessary to call this function once. Otherwise some patches to the runtime
@@ -16,6 +40,168 @@ fn main() -> Result<()> {
 
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
+
+    // 1. Initialize NVS
+    let _nvs_default_partition = EspDefaultNvsPartition::take()?;
+
+    // 2. Initialize bluetooth stack (blueroid)
+    let modem = unsafe { Modem::new() };
+    let (_, bt_modem) = modem.split();
+    let driver: BtDriver<Ble> = BtDriver::new(bt_modem, None)?;
+
+    // Obtain gatt and gap instances
+    let gatt = EspGatts::new(driver.borrow())?;
+    let gap: EspBleGap<Ble, &BtDriver<Ble>> = EspBleGap::new(driver.borrow())?;
+
+    let gatt_if_a: Arc<Mutex<Option<u8>>> = Arc::new(Mutex::new(None));
+    let gatt_if_b: Arc<Mutex<Option<u8>>> = Arc::new(Mutex::new(None));
+
+    // 3. Initialize gap and gatt callbacks
+    gap.subscribe(|event| {
+        log::info!("GAP Event: {:?}", event);
+    })?;
+
+    let (a_tmp, b_tmp) = (gatt_if_a.clone(), gatt_if_b.clone());
+    gatt.subscribe(move |(gatt_if, event)| {
+        log::info!("GATT Event: {:?} {:?}", gatt_if, event);
+
+        #[allow(clippy::single_match)]
+        match event {
+            GattsEvent::ServiceRegistered { status, app_id } => {
+                if status == GattStatus::Ok {
+                    if app_id == PROFILE_A_APP_ID {
+                        *a_tmp.lock().unwrap() = Some(gatt_if);
+                    } else if app_id == PROFILE_B_APP_ID {
+                        *b_tmp.lock().unwrap() = Some(gatt_if);
+                    }
+                }
+            }
+            _ => {}
+        };
+    })?;
+
+    // 4. Register GATT application profiles
+    gatt.register_app(PROFILE_A_APP_ID)?;
+    // gatt.register_app(PROFILE_B_APP_ID)?;
+
+    log::info!("GAP and GATT initialized");
+
+    // 5. Set advertising configuration
+    gap.set_adv_conf(&AdvConfiguration {
+        set_scan_rsp: true,
+        include_name: true,
+        include_txpower: true,
+        min_interval: 0,
+        max_interval: 0,
+        appearance: AppearanceCategory::ControlDevice,
+        service_uuid: Some(BtUuid::uuid128(28632491779680083979757574010553820695)),
+        service_data: None,
+        manufacturer_data: None,
+        ..Default::default()
+    })?;
+
+    log::info!("Set GAP advertising configuration");
+
+    // 6. Start advertising
+    gap.start_advertising()?;
+
+    log::info!("Started GAP advertising");
+
+    // 7. Create GATT service and characteristic
+    let service_id = GattServiceId {
+        id: GattId {
+            uuid: BtUuid::uuid128(248923085985797927161606609401596650095),
+            inst_id: 3,
+        },
+        is_primary: true,
+    };
+
+    let char_id = GattCharacteristic {
+        uuid: BtUuid::uuid128(313649116642616186956252207059869165533),
+        permissions: Permission::Read | Permission::Write,
+        properties: Property::Read | Property::Write,
+        max_len: 8,
+        auto_rsp: AutoResponse::ByApp,
+    };
+
+    log::info!("Prepared GATT Service and Characteristic IDs");
+
+    gatt.create_service(gatt_if_a.lock().unwrap().unwrap(), &service_id, 4)?;
+    // gatt.create_service(gatt_if_b.lock().unwrap().unwrap(), &service_id, 4)?;
+
+    log::info!("Created GATT Service");
+
+    gatt.start_service(40)?;
+    // gatt.start_service(44)?;
+
+    log::info!("Started GATT Service");
+
+    gatt.add_characteristic(40, &char_id, "1010".as_bytes())?;
+    // gatt.add_characteristic(44, &char_id, "AAAA".as_bytes())?;
+
+    log::info!("Added GATT Characteristic");
+
+    // gatt.start_service()?;
+
+    // gatt.create_service(PROFILE_A_APP_ID, &service_id, 1)?;
+
+    // log::info!("GAP and GATT initialized");
+
+    // log::info!("Starting GAP");
+
+    // gap.subscribe(|event| {
+    //     log::info!("GAP Event: {:?}", event);
+    // })?;
+
+    // log::info!("Subscribed to GAP events");
+
+    // gap.set_adv_conf(&AdvConfiguration::default())?;
+
+    // log::info!("Set GAP advertising configuration");
+
+    // gap.start_advertising()?;
+
+    // log::info!("Started GAP advertising");
+
+    // let service_id = GattServiceId {
+    //     id: GattId {
+    //         uuid: BtUuid::uuid128(241716678439078648716531568473180149994),
+    //         inst_id: 0,
+    //     },
+    //     is_primary: true,
+    // };
+
+    // let char_id = GattCharacteristic {
+    //     uuid: BtUuid::uuid128(8241668614845853411578711250896091271),
+    //     permissions: Permission::Read | Permission::Write,
+    //     properties: Property::Read | Property::Write,
+    //     max_len: 8,
+    //     auto_rsp: AutoResponse::ByApp,
+    // };
+
+    // log::info!("Prepared GATT Service and Characteristic IDs");
+
+    // // gatt.register_app(0)?;
+
+    // gatt.subscribe(|(gatt_if, event)| {
+    //     log::info!("GATT Event: {:?} {:?}", gatt_if, event);
+    // })?;
+
+    // log::info!("Subscribed to GATT events");
+
+    // gatt.create_service(0, &service_id, 1)?;
+
+    // log::info!("Created GATT Service");
+
+    // gatt.add_characteristic(0, &char_id, &[0, 0, 0, 0])?;
+
+    // log::info!("Added GATT Characteristic");
+
+    // gatt.start_service(0)?;
+
+    // log::info!("Started GATT Service");
+
+    // driver.
 
     // let gatt_server = esp32_gatt_ote::init_new_server()?;
 
