b797908ba9639a99afc010bf1247408fb712461f
dk731 <demid.kaidalov@gmail.com>
2024-05-24 16:11:43 +0300

    chore: Update crate names to adhere to naming conventions

--- esp-bluedroid/src/lib.rs
+++ esp-bluedroid/src/lib.rs
@@ -1,1 +1,7 @@
-
+lazy_static! {
+    pub static ref BT_DRIVER: StaticBtDriver =
+        BtDriver::new(unsafe { BluetoothModem::new() }, None).unwrap();
+    pub static ref GAP: StaticEspBleGap = Arc::new(EspBleGap::new((*BT_DRIVER).borrow()).unwrap());
+    pub static ref GATT: StaticEspGatts = Arc::new(EspGatts::new((*BT_DRIVER).borrow()).unwrap());
+    static ref OTA_BLE: Arc<Mutex<Option<Arc<OtaBle>>>> = Arc::new(Mutex::new(None));
+}
